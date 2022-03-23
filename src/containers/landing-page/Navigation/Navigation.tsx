7bfe59a5afa0e1a7330de7df7faf6fcbba75a1fc
Dust07 <tienphat0707@gmail.com>
2022-03-23 18:38:56 +0700

    Added Header Search Btn Func

--- src/containers/landing-page/Navigation/Navigation.tsx
+++ src/containers/landing-page/Navigation/Navigation.tsx
@@ -5,19 +5,24 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faBars,
   faMagnifyingGlass,
+  faXmark
 } from '@fortawesome/free-solid-svg-icons'
 import { navBar } from "../../../mockData/mockdata"
 import Category from './Category/Category'
 
-
-
 function Navigation() {
-  const [showNavBar, setShowNavBar] = useState(false);
+  const [showNavBar, setShowNavBar] = useState<boolean>(false);
+  const [showSearchField, setShowSearchField] = useState<boolean>(false);
+  
+  const handleSearchBtnOnClick = () => {
+    setShowSearchField(true);
+  }
+
   return (
     <div className="nav-container">
       <div className="nav-wrapper">
         <div className="dropDownBtn-wrapper">
-          <FontAwesomeIcon icon={faBars} className="dropDownBtn" onClick={() => { setShowNavBar(!showNavBar) }} />
+          <FontAwesomeIcon icon={faBars} className="dropDownBtn" onClick={() => setShowNavBar(!showNavBar) } />
         </div>
         <div className={`navbar-wrapper ${showNavBar && "show-nav-dropdown"}`}>
           <ul className="navbar">
@@ -29,8 +34,13 @@ function Navigation() {
           </ul>
         </div>
         <div className="searchBtn-wrapper">
-          <FontAwesomeIcon icon={faMagnifyingGlass} className="searchBtn" />
+          <FontAwesomeIcon icon={faMagnifyingGlass} className="searchBtn" onClick={handleSearchBtnOnClick}/>
         </div>
+        <div className={`header-search-field-wrapper ${showSearchField && "show"}`}>
+        <input className="header-search-field" type="text" name="searchText" id="searchText" placeholder="Nhập ít nhất 3 ký tự" />
+        <FontAwesomeIcon icon={faXmark} onClick={()=> setShowSearchField(false)} className="header-search-field-close-icon"/>
+        </div>
+        
       </div>
     </div>
 
