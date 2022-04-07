b8c8526aff7cd3e1cc3d2fba94f5301c6dcc7986
Dust07 <tienphat0707@gmail.com>
2022-04-07 11:12:05 +0700

    testing

--- src/redux/actions/users.tsx
+++ src/redux/actions/users.tsx
@@ -2,8 +2,5 @@ import { useDispatch } from "react-redux"
 import usersSlice from "../reducers/users"
 
 export const fetchUser = async () => {
-    return async (dispatch: any){
-        dispatch(usersSlice.actions.FETCH_USERS)
-
-    }
+   
 }
